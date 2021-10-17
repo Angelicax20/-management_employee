aa520107103887a7f8f9a6a6a9a4a72dbbf5e2a3
Manuel Garcia <manejarju15@gmail.com>
2021-10-17 18:56:03 -0500

    Vistas desarrolladas version-1

--- app.py
+++ app.py
@@ -1,17 +1,34 @@
-
-from flask import Flask, render_template
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-@app.route("/home")
-@app.route("/inicio")
+@app.route('/')
+@app.route('/index/')
+@app.route('/home/')
 
-def inicio():
-    return render_template("index.html")
+def home():
+    return render_template('index.html')
 
 
-if __name__ == "__main__":
+@app.route('/crear-usuario/')
+def crear_usuario():
+    return render_template('crear-usuario.html',titulo='Crear usuario')
+
+
+@app.route('/gestionar-usuario/')
+def gestionar():
+    return render_template('gestionar-usuario.html',titulo='Gestionar usuario')
+
+
+@app.route('/empleado/')
+def empleado():
+    return render_template('empleado.html',titulo='Empleado')
+
+
+@app.route('/login/')
+def login():
+    return render_template('login.html',titulo='login')
+
+if __name__ == '__main__':
     app.run(debug=True)
-
 
