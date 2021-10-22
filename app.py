774bdb584e48bd6fc8a457478570b8dca98fc59c
fonzk <alfdiazr@hotmail.com>
2021-10-22 12:01:48 -0500

    crear usuario-login

--- app.py
+++ app.py
@@ -1,17 +1,101 @@
-
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, session, flash, request
+from markupsafe import escape
+from werkzeug.security import check_password_hash, generate_password_hash
+from db import seleccion, accion
+from forms import Registro , Login as lg
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
-@app.route("/home")
-@app.route("/inicio")
+app.secret_key = os.urandom(24)
 
-def inicio():
-    return render_template("index.html")
+@app.route('/')
+@app.route('/index/')
+@app.route('/home/')
+def home():
+    return render_template('index.html')
 
 
-if __name__ == "__main__":
+
+
+
+@app.route('/crear-usuario/', methods=['GET', 'POST'])
+def crear_usuario():
+
+    """ V5. Utiliza almacenamiento seguro para los datos """
+    frm = Registro()
+    parametrosURL = {
+        'typeForm' : "Crear usuario",
+        'urlAction' : "/crear-usuario/",
+        'proceso' : ''
+    }
+
+    if request.method == 'GET':
+        return render_template('crear-usuario.html', prueba=frm, titulo='Registro de datos', parametros = parametrosURL)
+    else:
+        
+        nombres = escape(request.form['nombres'])
+        apellidos = escape(request.form['apellidos'])
+        fechaNacimiento = escape(request.form['fechaNacimiento'])
+        numeroDocumento = escape(request.form['numeroDocumento'])
+        tipoDoc = escape(request.form['tipoDoc'])
+        clave = escape(request.form['clave'])
+        telefono = escape(request.form['telefono'])
+        email = escape(request.form['email'])
+        salario = escape(request.form['salario'])
+        tipoContrato = escape(request.form['tipoContrato'])
+        fechaTerminoContrato = escape(request.form['fechaTerminoContrato'])
+        fechaIngreso = escape(request.form['fechaIngreso'])
+        cargo = escape(request.form['cargo'])
+        tipoUsuario = escape(request.form['tipoUsuario'])
+
+        swerror = False
+        if nombres==None or len(nombres)==0:
+            flash('ERROR: Debe suministrar un nombre de usuario')
+            swerror = True
+
+        if not swerror:
+             # Preparar el query -- Paramétrico
+
+            sql = "INSERT INTO Usuario (docIdentidad,nombre,apellidos,fechaNac,telefono,correo,tipoContrato,fechaTerContrato,salario) VALUES (?,?,?,?,?,?,?,?,?);"
+            # Ejecutar la consulta
+            clave = generate_password_hash(clave)
+
+            res = accion(sql, (numeroDocumento, nombres, apellidos, fechaNacimiento, telefono, email, tipoContrato, fechaTerminoContrato, salario))
+            # Proceso los resultados
+
+            if res==0:
+                parametrosURL['proceso'] = "insertado error"
+                parametrosURL['icon'] = "error"
+                parametrosURL['descripcion'] = "Intente insertar con otro numero de documento"
+            else:
+                parametrosURL['proceso'] = "insertado ok"
+                parametrosURL['icon'] = "success"
+                parametrosURL['descripcion'] = "Registro exitoso"
+
+        frm2 = Registro()
+        return render_template('crear-usuario.html', prueba=frm2, titulo='Registro de datos', parametros = parametrosURL)
+
+@app.route('/gestionar-usuario/')
+def gestionar():
+    return render_template('gestionar-usuario.html', titulo='Gestionar usuario')
+
+
+@app.route('/empleado/')
+def empleado():
+    return render_template('empleado.html', titulo='Empleado')
+
+
+@app.route('/login/',methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        frm_login = lg()
+        return render_template('login.html',prueba=frm_login)
+    else:
+        return home();
+    
+
+
+if __name__ == '__main__':
     app.run(debug=True)
-
 
---
+++ forms.py
@@ -0,0 +1,40 @@
+from flask_wtf import FlaskForm
+from wtforms import TextField, SubmitField, PasswordField, DateField, IntegerField, SelectField, DecimalField
+from wtforms.fields.html5 import EmailField, DateField, IntegerField, TelField, DecimalField
+from wtforms.validators import EqualTo, InputRequired
+
+class Login(FlaskForm):
+    usu = TextField('Usuario', validators = [InputRequired(message='Indique el usuario')])
+    cla = PasswordField('Contraseña', validators = [InputRequired(message='Indique la clave')])
+    btn = SubmitField('Ingresar')
+
+class Registro(FlaskForm):
+    nombres = TextField('Nombres', validators = [InputRequired(message='Ingrese un nombre')])
+
+    apellidos = TextField('Apellidos', validators = [InputRequired(message='Ingrese un apellido')])
+
+    fechaNacimiento = DateField('Fecha de nacimiento', validators = [InputRequired(message='Ingrese una fecha de nacimiento')])
+
+    numeroDocumento = IntegerField('Número de documento', validators = [InputRequired(message='Ingrese un numero de documento')])
+
+    tipoDoc = SelectField(u'Tipo de documento', choices=[('', ''),('1', 'Cedula de ciudadanía'), ('2', 'Tarjeta de identidad'), ('3', 'Pasaporte')], validators = [InputRequired(message='Seleccione un tipo de documento')])
+
+    clave = PasswordField('Contraseña', validators = [InputRequired(message='Ingrese una contraseña')])
+
+    telefono = TelField('Teléfono', validators = [InputRequired(message='Ingrese un teléfono')])
+
+    email = EmailField('Email *', validators = [InputRequired(message='Ingrese un email')])
+
+    salario = DecimalField('Salario', validators = [InputRequired(message='Ingrese un salario')])
+
+    tipoContrato = SelectField(u'Tipo de contrato', choices=[('', ''),('Termino fijo','Termino fijo'),('Termino indefinido','Termino indefinido'), ('Obra o labor','Obra o labor'), ('Aprendizaje','Aprendizaje'), ('Temporal ocasional o accidental','Temporal ocasional o accidental'), ('Prestación de servicios','Prestación de servicios')], validators = [InputRequired(message='Seleccione un tipo de contrato')])
+
+    fechaTerminoContrato = DateField('Fecha termino de contrato', validators = [InputRequired(message='Ingrese una fecha de cierre de contrato')])
+    
+    fechaIngreso = DateField('Fecha termino de ingreso', validators = [InputRequired(message='Ingrese una fecha de ingreso')])
+
+    cargo = TextField('Cargo', validators = [InputRequired(message='Ingrese un cargo')])
+
+    tipoUsuario = SelectField(u'Tipo de usuario', choices=[('', ''),('admin', 'Administrador'), ('empleado', 'Empleado')], validators = [InputRequired(message='Seleccione el tipo de usuario')])
+
+    btnEnviar = SubmitField('Crear usuario')
